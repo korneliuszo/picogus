df5e21b690698aec9135c982a4362ce3ec3dea84
Korneliusz Osmenda <korneliuszo@gmail.com>
2024-08-06 20:15:07 +0200

    Cut out ISA_IO specific procedures

--- sw/picogus.cpp
+++ sw/picogus.cpp
@@ -20,12 +20,18 @@
 
 board_type_t BOARD_TYPE;
 
+constexpr uint32_t rp2_clock = RP2_CLOCK_SPEED;
+constexpr float psram_clkdiv = (float)rp2_clock / 200000.0;
+constexpr float pwm_clkdiv = (float)rp2_clock / 22727.27;
+
+
 #ifdef PSRAM
 #include "psram_spi.h"
 psram_spi_inst_t psram_spi;
 psram_spi_inst_t* async_spi_inst;
 #endif
-#include "isa_io.pio.h"
+
+#include "isa_io.h"
 
 #ifdef ASYNC_UART
 #include "stdio_async_uart.h"
@@ -123,9 +129,6 @@ static bool queueReboot = false;
 
 Settings settings;
 void processSettings(void);
-
-#define IOW_PIO_SM 0
-#define IOR_PIO_SM 1
 
 const char* firmware_string = PICO_PROGRAM_NAME " v" PICO_PROGRAM_VERSION_STRING;
 
@@ -816,20 +819,6 @@ __force_inline void handle_ior(void) {
     }
 }
 
-#ifdef USE_IRQ
-void iow_isr(void) {
-    /* //printf("ints %x\n", pio0->ints0); */
-    handle_iow();
-    // pio_interrupt_clear(pio0, pio_intr_sm0_rxnempty_lsb);
-    irq_clear(PIO0_IRQ_0);
-}
-void ior_isr(void) {
-    handle_ior();
-    // pio_interrupt_clear(pio0, PIO_INTR_SM0_RXNEMPTY_LSB);
-    irq_clear(PIO0_IRQ_1);
-}
-#endif
-
 void err_blink(void) {
     for (;;) {
         //gpio_xor_mask(LED_PIN);//need to abscrat  led functions out to handle chipdown vs  picow
@@ -840,21 +829,6 @@ void err_blink(void) {
 #ifndef USE_ALARM
 #include "pico_pic.h"
 #endif
-
-constexpr uint32_t rp2_clock = RP2_CLOCK_SPEED;
-constexpr float psram_clkdiv = (float)rp2_clock / 200000.0;
-constexpr float pwm_clkdiv = (float)rp2_clock / 22727.27;
-constexpr float iow_clkdiv = (float)rp2_clock / 183000.0;
-
-constexpr uint32_t iow_rxempty = 1u << (PIO_FSTAT_RXEMPTY_LSB + IOW_PIO_SM);
-__force_inline bool iow_has_data() {
-    return !(pio0->fstat & iow_rxempty);
-}
-
-constexpr uint32_t ior_rxempty = 1u << (PIO_FSTAT_RXEMPTY_LSB + IOR_PIO_SM);
-__force_inline bool ior_has_data() {
-    return !(pio0->fstat & ior_rxempty);
-}
 
 #include "hardware/structs/xip_ctrl.h"
 int main()
@@ -1066,51 +1040,10 @@ extern void PIC_DeActivateIRQ(void);
     multicore_launch_core1(&play_usb);
 #endif // USBONLY
 
-    for(int i=AD0_PIN; i<(AD0_PIN + 10); ++i) {
-        gpio_disable_pulls(i);
-    }
-    gpio_disable_pulls(IOW_PIN);
-    gpio_disable_pulls(IOR_PIN);
-    gpio_pull_down(IOCHRDY_PIN);
-    gpio_set_dir(IOCHRDY_PIN, GPIO_OUT);
-
-    puts("Enabling bus transceivers...");
-    // waggle ADS to set BUSOE latch
-    gpio_init(ADS_PIN);
-    gpio_set_dir(ADS_PIN, GPIO_OUT);
-    gpio_put(ADS_PIN, 1);
-    busy_wait_ms(10);
-    gpio_put(ADS_PIN, 0);
-
-    puts("Starting ISA bus PIO...");
-    // gpio_set_drive_strength(ADS_PIN, GPIO_DRIVE_STRENGTH_12MA);
-    gpio_set_slew_rate(ADS_PIN, GPIO_SLEW_RATE_FAST);
-
-    uint iow_offset = pio_add_program(pio0, &iow_program);
-    pio_sm_claim(pio0, IOW_PIO_SM);
-    printf("iow sm: %u\n", IOW_PIO_SM);
-
-    uint ior_offset = pio_add_program(pio0, &ior_program);
-    pio_sm_claim(pio0, IOR_PIO_SM);
-    printf("ior sm: %u\n", IOR_PIO_SM);
-
-    iow_program_init(pio0, IOW_PIO_SM, iow_offset, iow_clkdiv);
-    ior_program_init(pio0, IOR_PIO_SM, ior_offset);
+    isa_prepare();
 
 #ifdef USE_IRQ
-    puts("Enabling IRQ on ISA IOR/IOW events");
-    // iow irq
-    irq_set_enabled(PIO0_IRQ_0, false);
-    pio_set_irq0_source_enabled(pio0, pis_sm0_rx_fifo_not_empty, true);
-    irq_set_priority(PIO0_IRQ_0, PICO_HIGHEST_IRQ_PRIORITY);
-    irq_set_exclusive_handler(PIO0_IRQ_0, iow_isr);
-    irq_set_enabled(PIO0_IRQ_0, true);
-    // ior irq
-    irq_set_enabled(PIO0_IRQ_1, false);
-    pio_set_irq1_source_enabled(pio0, pis_sm1_rx_fifo_not_empty, true);
-    irq_set_priority(PIO0_IRQ_1, PICO_HIGHEST_IRQ_PRIORITY);
-    irq_set_exclusive_handler(PIO0_IRQ_1, ior_isr);
-    irq_set_enabled(PIO0_IRQ_1, true);
+    isa_irq_prepare();
 #endif
 
     gpio_xor_mask(LED_PIN);
@@ -1123,13 +1056,7 @@ extern void PIC_DeActivateIRQ(void);
 
     for (;;) {
 #ifndef USE_IRQ
-        if (iow_has_data()) {
-            handle_iow();
-        }
-
-        if (ior_has_data()) {
-            handle_ior();
-        }
+    	isa_poll();
 #endif
 #ifndef USE_ALARM
         PIC_HandleEvents();
